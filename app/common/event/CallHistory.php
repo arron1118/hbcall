8b79731721b6aa92562151443a2ba8f3be81b5a2
arron1118 <arron1118@icloud.com>
2021-06-17 17:04:01 +0800

    'update'

--- app/common/event/CallHistory.php
+++ app/common/event/CallHistory.php
@@ -24,7 +24,7 @@ class CallHistory
         }
 
         $HistoryModel = new \app\common\model\CallHistory();
-        $callList = $HistoryModel->where($map)->order('id DESC')->limit(10)->select();
+        $callList = $HistoryModel->where($map)->order('id DESC')->limit(20)->select();
         if (!empty($callList)) {
             $curl = new Curl();
             $news = [];
