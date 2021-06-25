572174180bfd0eb569f05b2d09ddc939d8eb1def
arron1118 <arron1118@icloud.com>
2021-06-25 17:32:46 +0800

    'update'

--- app/company/controller/Payment.php
+++ app/company/controller/Payment.php
@@ -20,14 +20,44 @@ class Payment extends \app\common\controller\CompanyController
 
     public function index()
     {
-        $res1 = Pay::wechat(Config::get('wxpay'))->find(['out_trade_no' => '202106241624521612005893']);
-        $res2 = Pay::wechat(Config::get('wxpay'))->find(['out_trade_no' => '202106241624498668248659']);
-        dump($res1);
-        dump($res2);
+//        $res2 = Pay::wechat(Config::get('wxpay'))->find(['out_trade_no' => '202106241624498668248659']);
+//        dump($res2);
+        $nopay = $this->model->where(['company_id' => Session::get('company.id'), 'payment_no' => ''])->select();
+        foreach ($nopay as $key => $value) {
+            $data = Pay::wechat(Config::get('wxpay'))->find(['out_trade_no' => $value->payno]);
+            if ($data->trade_state === 'SUCCESS') {
+                $mt = mktime(
+                    substr($data->time_end, 8, 2),
+                    substr($data->time_end, 10, 2),
+                    substr($data->time_end, 12, 2),
+                    substr($data->time_end, 4, 2),
+                    substr($data->time_end, 6, 2),
+                    substr($data->time_end, 0, 4)
+                );
+                $paymentModel = $this->model->where('payno', $data->out_trade_no)->find();
+                $paymentModel->pay_time = $mt;
+                $paymentModel->payment_no = $data->transaction_id;
+                $paymentModel->status = 1;
+                $paymentModel->save();
+            }
+        }
+
         return $this->view->fetch();
     }
 
-    public function find()
+    public function getOrderList()
+    {
+        if ($this->request->isPost()) {
+            $page = (int) $this->request->param('page', 1);
+            $limit = (int) $this->request->param('limit', 10);
+            $total = $this->model->count();
+
+            $historyList = $this->model->order('id DESC')->limit(($page - 1) * $limit, $limit)->select();
+            return json(['rows' => $historyList, 'total' => $total, 'msg' => '', 'code' => 1]);
+        }
+    }
+
+    public function checkOrder()
     {
         $res = Pay::wechat(Config::get('wxpay'))->find(['out_trade_no' => '202106241624498668248659']);
         return json($res);
--- app/company/model/Payment.php
+++ app/company/model/Payment.php
@@ -14,9 +14,30 @@ class Payment extends \think\Model
 
     public function getPayTimeAttr($value)
     {
+        if (!$value) {
+            return '-';
+        }
         return date($this->getDateFormat(), $value);
     }
 
+    public function getPayTypeAttr($value)
+    {
+        return $this->getPayTypeList()[$value];
+    }
 
+    public function getPayTypeList()
+    {
+        return [1 => '微信', 2 => '支付宝'];
+    }
+
+    public function getStatusAttr($value)
+    {
+        return $this->getStatusList()[$value];
+    }
+
+    public function getStatusList()
+    {
+        return ['未支付', '已支付'];
+    }
 
 }
