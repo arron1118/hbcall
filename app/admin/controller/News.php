2b26996e1ae8d5a374b904b2c7ee0288f602c4d5
lvqiuping <448427168@qq.com>
2021-08-09 19:05:13 +0800

    5555

---
+++ app/admin/controller/News.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace app\admin\controller;
+
+
+class News extends \app\common\controller\AdminController
+{
+
+    public function index()
+    {
+        return $this->view->fetch();
+    }
+
+    public function add()
+    {
+        return $this->view->fetch();
+    }
+
+    public function edit()
+    {
+        return $this->view->fetch();
+    }
+
+    public function delete()
+    {
+        return $this->view->fetch();
+    }
+}
