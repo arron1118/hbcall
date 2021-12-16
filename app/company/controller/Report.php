5eb6e1638b9e8a200cfbf4a3079c241f1fe4f81d
arron1118 <arron1118@icloud.com>
2021-12-16 18:58:14 +0800

    更新搜索

--- app/company/controller/Report.php
+++ app/company/controller/Report.php
@@ -9,39 +9,41 @@ class Report extends \app\common\controller\CompanyController
 
     public function index()
     {
-        $user = $this->userInfo;
-        $sql = <<<SQL
-select u.id, u.username, c.corporation, total, total1, total2
-from hbcall_user u
-         left join hbcall_company c on u.company_id=c.id
-         left join (select count(id) as total, user_id
-                    from hbcall_call_history h
-                    group by user_id) ch on u.id = ch.user_id
-left join (select count(id) as total1, user_id from hbcall_call_history where call_duration > 0 group by user_id) lch on lch.user_id=u.id
-left join (select count(id) as total2, user_id from hbcall_call_history where call_duration > 30 group by user_id) gch on gch.user_id=u.id
-where company_id={$user['id']}
-order by total desc
-SQL;
-        $data = Db::query($sql);
-        $this->view->assign(['reportData' => $data]);
         return $this->view->fetch();
     }
 
     public function getReport()
     {
+        $startDate = $this->request->param('startDate', '');
+        $endDate = $this->request->param('endDate', '');
+        $where = ' ';
+
+        if ($startDate && !$endDate) {
+            $where .= ' and createtime > ' . strtotime($startDate);
+        }
+
+        if (!$startDate && $endDate) {
+            $where .= ' and createtime < ' . strtotime($endDate);
+        }
+
+        if ($startDate && $endDate) {
+            $where .= ' and (createtime between ' . strtotime($startDate) . ' and ' . strtotime($endDate) . ')';
+        }
+
         $user = $this->userInfo;
         $sql = <<<SQL
 select u.id, u.username, c.corporation, total, total1, total2
 from hbcall_user u
          left join hbcall_company c on u.company_id=c.id
          left join (select count(id) as total, user_id
-                    from hbcall_call_history h
+                    from hbcall_call_history h where 1=1  {$where}
                     group by user_id) ch on u.id = ch.user_id
-left join (select count(id) as total1, user_id from hbcall_call_history where call_duration > 0 group by user_id) lch on lch.user_id=u.id
-left join (select count(id) as total2, user_id from hbcall_call_history where call_duration > 30 group by user_id) gch on gch.user_id=u.id
+left join (select count(id) as total1, user_id from hbcall_call_history where call_duration > 0 {$where} group by user_id) lch on lch.user_id=u.id
+left join (select count(id) as total2, user_id from hbcall_call_history where call_duration > 30 {$where} group by user_id) gch on gch.user_id=u.id
 where company_id={$user['id']}
 order by total desc
 SQL;
+
         $this->returnData['code'] = 1;
         $this->returnData['data'] = Db::query($sql);
         $this->returnData['msg'] = 'success';
