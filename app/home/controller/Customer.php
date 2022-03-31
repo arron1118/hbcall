0314ba51aefedef7d157e4ff3b9f8192408fd4c0
arron1118 <arron1118@icloud.com>
2022-03-31 18:51:34 +0800

    update

--- app/home/controller/Customer.php
+++ app/home/controller/Customer.php
@@ -14,19 +14,36 @@ class Customer extends \app\common\controller\HomeController
     public function getCustomerList()
     {
         if ($this->request->isPost()) {
+            $page = (int)$this->request->param('page', 1);
+            $limit = (int)$this->request->param('limit', 10);
+            $title = trim($this->request->param('title', ''));
+            $phone = trim($this->request->param('phone', ''));
+
             $where = [
-                'user_id' => $this->userInfo->id
+                ['user_id', '=', $this->userInfo->id]
             ];
+
+            if ($title) {
+                $where[] = ['title', 'like', '%' . $title . '%'];
+            }
+
+            if ($phone) {
+                $where[] = ['phone', 'like', '%' . $phone . '%'];
+            }
+
+            $total = CustomerModel::where($where)->count();
 
             $res = CustomerModel::field('id, title, phone, called_count, last_calltime')
                 ->where($where)
                 ->order('called_count')
                 ->order('id', 'desc')
+                ->limit(($page - 1) * $limit, $limit)
                 ->select();
 
             $this->returnData['data'] = $res;
             $this->returnData['code'] = 1;
             $this->returnData['msg'] = 'success';
+            $this->returnData['total'] = $total;
 
             return json($this->returnData);
         }
