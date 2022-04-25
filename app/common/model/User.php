434b512cc3a8486867e63e9783bde54f9673af2c
arron1118 <arron1118@icloud.com>
2022-04-25 11:00:44 +0800

    update

--- app/common/model/User.php
+++ app/common/model/User.php
@@ -27,7 +27,7 @@ class User extends \think\Model
 
     public function getTestEndtimeAttr($value)
     {
-        return getDateFormatInfo($value);
+        return date($this->getDateFormat(), $value);
     }
 
     protected function getStatusList()
