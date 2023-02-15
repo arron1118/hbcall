e3dd01bc93873fe0f2e46719576825180c8babee
arron1118 <arron1118@icloud.com>
2023-02-15 14:17:26 +0800

    update

--- config/app.php
+++ config/app.php
@@ -11,7 +11,7 @@ return [
     // 是否启用路由
     'with_route'       => true,
     // 默认应用
-    'default_app'      => 'portal',
+    'default_app'      => 'home',
     // 默认时区
     'default_timezone' => 'Asia/Shanghai',
 
@@ -23,7 +23,7 @@ return [
     // 域名绑定（自动多应用模式有效）
     'domain_bind'      => [],
     // 禁止URL访问的应用列表（自动多应用模式有效）
-    'deny_app_list'    => ['common'],
+    'deny_app_list'    => ['common', 'portal'],
 
     // 异常页面的模板文件
     'exception_tmpl'   => app()->getThinkPath() . 'tpl/think_exception.tpl',
