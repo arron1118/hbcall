6fee6a58776a9433a8dbee8b1a9e202c1f3847fb
arron1118 <arron1118@icloud.com>
2023-12-27 10:54:33 +0800

    添加定时任务

---
+++ syncCallHistory/sync.bat
@@ -0,0 +1,1 @@
+"D:\phpstudy_pro\Extensions\php\php7.4.3nts\php.exe" -f "D:\phpstudy_pro\WWW\hbcall\syncCallHistory\sync.php"
---
+++ syncCallHistory/sync.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * copyright@Administrator
+ * 2023/12/27 0027 10:45
+ * email:arron1118@icloud.com
+ */
+set_time_limit(0);
+file_get_contents('http://caller.hbosw.com/api/hbcall/updateCallHistory');
