696b72bb1392f51775187866528ffb031edeb9f7
arron1118 <arron1118@icloud.com>
2025-01-09 10:12:51 +0800

    update

--- config/hbcall.php
+++ config/hbcall.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'call_api' => 'http://call.hbosw.net/API/axbCallApi.aspx',
+    'call_api' => 'http://call.miaotouying.com/API/axbCallApi.aspx',
     'record_api' => 'http://phone.hbosw.cn:8093/api/Call/GetCallRecordByBindId',
-    'axb_call_api' => 'http://call.hbosw.net/API/CallPhoneApi.aspx',
+    'axb_call_api' => 'http://call.miaotouying.com/API/CallPhoneApi.aspx',
     // 电信回拨记录地址
     'dx_record_api' => 'http://phone.hbosw.cn:8093/api/Call/GetCallRecordByBindId',
 ];
